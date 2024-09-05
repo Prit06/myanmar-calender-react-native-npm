35acd0dee30131e375ea303345ad1ee46ef876a1
shrutiponkiya <shrutiponkiya.technocomet01@gmail.com>
2024-09-05 15:59:45 +0530

    fix code with ads

--- App.js
+++ App.js
@@ -1,9 +1,59 @@
 
+// import React, { useEffect } from 'react'
+// import { GestureHandlerRootView } from 'react-native-gesture-handler';
+// // import {  NavigationContainer} from '@react-navigation/native';
+// import DrawerNavigation from './src/DrawerNavigation';
+// import SplashScreen from 'react-native-splash-screen';
+
+// const App = () => {
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       SplashScreen.hide();
+//     }, 1000);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <GestureHandlerRootView style={{flex: 1}}>
+    
+//       <DrawerNavigation />
+    
+//     </GestureHandlerRootView>
+//   )
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useEffect } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 // import {  NavigationContainer} from '@react-navigation/native';
 import DrawerNavigation from './src/DrawerNavigation';
 import SplashScreen from 'react-native-splash-screen';
+import { AdProvider } from './src/adsContext';
 
 const App = () => {
 
@@ -17,8 +67,9 @@ const App = () => {
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-    
-      <DrawerNavigation />
+      <AdProvider>
+        <DrawerNavigation />
+      </AdProvider>
     
     </GestureHandlerRootView>
   )
---
+++ src/adsContext.js
@@ -0,0 +1,16 @@
+import React, { useState, createContext, useContext } from 'react';
+
+export const AdContext = createContext();
+
+export const AdProvider = ({ children }) => {
+  const [adCount, setAdCount] = useState(0);
+
+  const incrementAdCount = () => setAdCount(adCount + 1);
+  const resetAdCount = () => setAdCount(0);
+
+  return (
+    <AdContext.Provider value={{ adCount, incrementAdCount, resetAdCount }}>
+      {children}
+    </AdContext.Provider>
+  );
+};
